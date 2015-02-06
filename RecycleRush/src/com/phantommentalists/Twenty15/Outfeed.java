30b0b1459dc837277e0e3e0f479ce48a73c9f972
ctheace <ctheace@gmail.com>
2015-02-06 15:07:28 -0500

    First Commit

--- RecycleRush/src/com/phantommentalists/Twenty15/Outfeed.java
+++ RecycleRush/src/com/phantommentalists/Twenty15/Outfeed.java
@@ -1,66 +1,64 @@
 package com.phantommentalists.Twenty15;
 
-import java.util.Vector;
-import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.ControlMode;
 import edu.wpi.first.wpilibj.DigitalInput;
 
 /*
+ * Author: Christopher D. Hooks
  */
 public class Outfeed {
 
-      private VictorSP pusher;
-    private VictorSP roller;
-    private DigitalInput leftLimit;
-    private DigitalInput rightLimit;
+    private CANTalon pusher;
+    private CANTalon roller;
     private DigitalInput toteOut;
-    public Vector  outfeedDeploy;
 
+    public Outfeed(){
+    	toteOut = new DigitalInput(Parameters.outfeedToteLimit);
+    	
+    	pusher=new CANTalon(Parameters.outfeedArmCANId);
+    	roller=new CANTalon(Parameters.outfeedConveyorCANId);
+    	
+    	pusher.changeControlMode(ControlMode.Voltage);
+    	roller.changeControlMode(ControlMode.Voltage);
+    	
+    	pusher.enableLimitSwitch(true, true);
+    	
+    }
+    
   /** 
    *  This method will move a stack forward in the outfeed.
    */
   public void moveStackForward() {
+	  roller.set(Parameters.outfeedConveyorVoltage);
   }
 
   /** 
    *  This method moves the leadscrew left to push a stack left.
    */
   public void moveStackLeft() {
-        /**
-         * pusher.set(1.0)
-         */
+	  pusher.set(-Parameters.outfeedPusherVoltage);
   }
 
   /** 
    *  This method moves the leadscrew right to push a stack right.
    */
   public void moveStackRight() {
+	  pusher.set(Parameters.outfeedPusherVoltage);
   }
 
   /** 
    *  This method returns true if the stack has left the outfeed, false otherwise.
    */
   public boolean isStackAllTheWayOut() {
-  return false;
+	  return toteOut.get();
   }
-
-  /** 
-   *  This method retracts the pushing arm.
-   *  NOTE : MAY NOT BE USED/NECESSARY.
-   */
-  public void retractPusher() {
-  }
-
-  /** 
-   *  This method deploys the outfeed.
-   */
-  public void deployOutfeed() {
-  }
-
+  
   /** 
    *  This method returns true if a stack is in the outfeed, false otherwise.
    */
   public boolean isStackInOutfeed() {
-  return false;
+	  return toteOut.get();
   }
 
 }
--- RecycleRush/src/com/phantommentalists/Twenty15/Parameters.java
+++ RecycleRush/src/com/phantommentalists/Twenty15/Parameters.java
@@ -1,19 +1,31 @@
 package com.phantommentalists.Twenty15;
 
 public class Parameters {
+	
+  public static final int frontLeftCanId = 11;
 
-  public static final int frontDriveCanId = 0;
+  public static final int frontRightCanId = 12;
 
-  public static final int frontLeftSteeringCanId = 0;
+  public static final int rearLeftCanId = 13;
 
-  public static final int frontRightCanId = 0;
-
-  public static final int rearCanId = 0;
-
-  public static final int rearLeftSteeringCanId = 0;
-
-  public static final int rearRightSteeringCanId = 0;
-
+  public static final int rearRightCanId = 14;
+  
+  public static final int stackerLeftCANId = 20;
+  
+  public static final int stackerRightCANId = 21;
+  
+  public static final int stackerConveyorCANId = 22;
+  
+  /**
+   * Outfeed Values
+   */
+  public static final int outfeedConveyorCANId = 30;
+  public static final int outfeedArmCANId = 31;
+  public static final int outfeedToteLimit = -1;
+  public static final double outfeedConveyorVoltage = 12;
+  public static final double outfeedPusherVoltage = 12;
+  
+  
   public static final int steeringProportionalValue = 0;
 
   public static final int steeringIntegralValue = 0;
