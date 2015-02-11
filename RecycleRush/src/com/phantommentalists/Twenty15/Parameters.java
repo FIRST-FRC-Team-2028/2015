a899fb37a97ef30cc5f70a3a82ad46400df8106c
Justin <justintime@verizon.net>
2015-02-11 16:21:56 -0500

    Finishing merge

--- RecycleRush/src/com/phantommentalists/Twenty15/Parameters.java
+++ RecycleRush/src/com/phantommentalists/Twenty15/Parameters.java
@@ -1,5 +1,7 @@
 package com.phantommentalists.Twenty15;
 
+import edu.wpi.first.wpilibj.Joystick.AxisType;
+
 public class Parameters {
 	
   public static final int frontLeftCanId = 11;
@@ -10,22 +12,21 @@ public class Parameters {
 
   public static final int rearRightCanId = 14;
   
-  /**
-   * Outfeed Values
-   */
-  public static final int outfeedConveyorCANId = 30;
-  public static final int outfeedArmCANId = 31;
+  public static final int stackerLeftCANId = 23;
+  
+  public static final int stackerRightCANId = 22;
+  
+  public static final int stackerConveyorCANId = 21;
+  
+  public static final int infeedCANId = 40;
+  
+//  Outfeed Values
+   
+  public static final int outfeedConveyorCANId = 31;
+  public static final int outfeedArmCANId = 30;
   public static final int outfeedToteLimitSwitch = -1;
   public static final double outfeedConveyorVoltage = 12;
   public static final double outfeedPusherVoltage = 12;
-
-  //Stacker Values
-  public static final int stackerLeftCANId = 22;
-  public static final int stackerRightCANId = 23;
-  public static final int stackerConveyorCANId = 21;
-  public static final double stackerConveyorVoltage = 1.0;
-  public static final double stackerElevatorUpVoltage = 0.5;
-  public static final double stackerElevatorDownVoltage = -stackerElevatorUpVoltage;
 
   public static final int steeringProportionalValue = 0;
 
@@ -34,6 +35,31 @@ public class Parameters {
   public static final int steeringDerivativeValue = 0;
 
   public static final double maxMotorVoltage = 12.0;
-
   
+  public static final double stackerConveyorVoltage = 1;
+  
+  //Button Values by Joystick
+  
+  	//GameMech
+  	public static final int buttonPusherLeft = 1;
+  	
+  	public static final int buttonPusherRight = 2;
+  	
+  	public static final int buttonOutfeedConveyorFwd = 8;
+  	
+  	public static final int buttonOutfeedConveyorRev = 7;
+  	
+  	public static final int buttonStackerUp = 4;
+  	
+  	public static final int buttonStackerDown = 3;
+  	
+  	public static final int buttonStackerConveyorFwd = 10;
+  	
+  	public static final int buttonStackerConveyorRev = 9;
+  	
+  	public static final int buttonInfeedUp = 6;
+  	
+  	public static final int buttonInfeedDown = 5;
+  	
+  	public static final AxisType axisInfeedIndicator = AxisType.kX;
 }
